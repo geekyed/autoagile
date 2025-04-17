4b0e7f7f217cad23827d2a04e96d2039119e8fac
geekyed <eddweston@me.com>
2025-04-17 10:50:56 +0100

    Auth working

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Button from "../../../lib/components/ui/button/button.svelte";
+
+
+	import { Card } from "../../../lib/components/ui/card";
+	import CardContent from "../../../lib/components/ui/card/card-content.svelte";
+	import CardHeader from "../../../lib/components/ui/card/card-header.svelte";
+</script>
+
+<Card>
+  <CardHeader>
+    Login to the site!
+  </CardHeader>
+  <CardContent>
+
+      <Button href="/auth/login/github">sign in with github</Button>
+
+  </CardContent>
+</Card>
