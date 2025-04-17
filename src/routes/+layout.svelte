bad508aac9e09feb8867f984b5e4ac3906d3f011
geekyed <eddweston@me.com>
2025-04-17 08:54:33 +0100

    added shad ui components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,19 @@
-<script lang='ts'>
-  import { invalidate } from '$app/navigation'
+<script lang="ts">
+	import '../app.css';
+	import { invalidate } from '$app/navigation';
 
-  let { data: propsData, children } = $props()
-  let { session, supabase } = $derived(propsData)
+	let { data: propsData, children } = $props();
+	let { session, supabase } = $derived(propsData);
 
-  $effect(() => {
-    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
-      if (newSession?.expires_at !== session?.expires_at) {
-        invalidate('supabase:auth')
-      }
-    })
+	$effect(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
 
-    return () => data.subscription.unsubscribe()
-  })
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <div>
