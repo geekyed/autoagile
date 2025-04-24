1a91e5c2ca767fd52cfa996555d3b299090a2333
geekyed <eddweston@me.com>
2025-04-24 20:38:06 +0100

    nav bar items

--- src/routes/navigation.svelte
+++ src/routes/navigation.svelte
@@ -14,13 +14,22 @@
     <ul  class="flex flex-row m-2 gap-5">
       <li>
         <a href="/">
-          <span class="flex-auto">Devices</span>
+          <span class="flex-auto">Home</span>
         </a>
       </li>
-
       <li>
         <a href="/profile">
           <span class="flex-auto">Profile</span>
+        </a>
+      </li>
+      <li>
+        <a href="/car-charging">
+          <span class="flex-auto">Car Charge</span>
+        </a>
+      </li>
+      <li>
+        <a href="/smart-devices">
+          <span class="flex-auto">Smart Devices</span>
         </a>
       </li>
     </ul>
