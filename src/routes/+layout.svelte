3752635fc3add34566cf1dc921414576223539e8
geekyed <eddweston@me.com>
2025-05-02 20:31:06 +0100

    fix title for mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,17 +20,19 @@
 	});
 </script>
 
-<div class="relative flex items-center justify-center p-4">
-  <h1 class="text-3xl font-semibold">Octomiser</h1>
-  <div class="absolute right-4 flex gap-2">
+<div class="flex items-center justify-between p-4 gap-4">
+  <div class="w-10 sm:w-24"></div>
+  <h1 class="flex-grow text-center text-4xl font-semibold">Octomiser</h1>
+  <div class="flex gap-2 items-center">
     {#if userProfile === null}
-			<Button variant='secondary' href="/auth/login/github">Sign in with GitHub</Button>
-		{:else}
-			<Button variant='secondary' href="/auth/logout">Logout</Button>
-		{/if}
-		<DarkMode/>
+      <Button variant="secondary" href="/auth/login/github">Sign in with GitHub</Button>
+    {:else}
+      <Button variant="secondary" href="/auth/logout">Logout</Button>
+    {/if}
+    <DarkMode />
   </div>
 </div>
+
 {#if userProfile}
 	<Navigation/>
 {/if}
