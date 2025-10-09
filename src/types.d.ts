02a271f996d01fd282aecfc5095e823d698e321d
geekyed <eddweston@me.com>
2025-10-09 11:12:02 +0100

    Start

--- src/types.d.ts
+++ src/types.d.ts
@@ -47,3 +47,31 @@ interface PriceChanges {
   inserts: Price[] | undefined;
   deletes: Partial<Price>[] | undefined;
 }
+
+// Octopus
+interface MeterPoint {
+  tariff_code: string;
+  valid_from: string;
+  valid_to: string | null;
+}
+
+interface ElectricityMeterPoint {
+  mpan: string;
+  agreements: MeterPoint[];
+}
+
+interface Property {
+  electricity_meter_points: ElectricityMeterPoint[];
+}
+
+interface AccountData {
+  properties: Property[];
+}
+
+interface StandardUnitRate {
+  value_exc_vat: number;
+  value_inc_vat: number;
+  valid_from: string; // ISO 8601 date-time string
+  valid_to: string; // ISO 8601 date-time string
+  payment_method: string | null;
+}
